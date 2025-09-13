a22bd4ec9ebacac9c923db4e3b8507dd96f89e73
hdsouza13 <hdsouza@ucsd.edu>
2025-09-13 01:12:33 -0700

    Experiencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Experiences from "./sections/Experiences"
 
 const App = () => {
   return <div className='container mx-auto max-w-7xl'>
     <Navbar />
     <Hero />
     <About />
-    <section className="min-h-screen"/>
-    <section className="min-h-screen"/>
+    <Projects />
+    <Experiences />
     <section className="min-h-screen"/>
     <section className="min-h-screen"/>
     {/* projects */}
--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 const CopyEmailButton = () => {
   const [copied, setCopied] = useState(false);
-  const email = "Your Email Address";
+  const email = "hugo.xvsouza@yahoo.com.br";
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(email);
