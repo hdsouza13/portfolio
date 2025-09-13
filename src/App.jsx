63615ad9b1c85b006c1342ef9284d39f2313db3d
hdsouza13 <hdsouza@ucsd.edu>
2025-09-12 23:08:36 -0700

    About Me

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return <div className='container mx-auto max-w-7xl'>
     <Navbar />
     <Hero />
-    {/* about */}
+    <About />
+    <section className="min-h-screen"/>
+    <section className="min-h-screen"/>
+    <section className="min-h-screen"/>
+    <section className="min-h-screen"/>
     {/* projects */}
     {/* experience */}
     {/* testimonial */}
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,7 +7,6 @@ import { useMediaQuery } from "react-responsive";
 import { easing } from "maath";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
-import { OrbitControls } from "@react-three/drei";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
@@ -26,7 +25,6 @@ const Hero = () => {
                 scale={isMobile && 0.23}
                 position={isMobile && [0, -1.5, 0]}
               />
-              <OrbitControls />
             </Float>
             <Rig />
           </Suspense>
